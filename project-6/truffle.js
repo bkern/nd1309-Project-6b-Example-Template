7c43808e3ba6a5eed6a8e004b7ca373e1506340e
Barry Kern <bskern@Barrys-Mac-mini.local>
2021-09-03 06:38:32 -0500

    work

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,28 @@
+
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ const infuraKey = "fb85aac4c13c4955ba95b9fff5563d60";
+ 
+ const fs = require('fs');
+ const mnemonic = fs.readFileSync(".secret").toString().trim(); // gitignore
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 5500000,        // rinkeby has a lower block limit than mainnet
+        confirmations: 2,
+        timeoutBlocks: 200,
+        skipDryRun: true,
+    },
   }
+    //  compilers: {
+    // solc: {
+    //     version: "0.4.24" // ex:  "0.4.20". (Default: Truffle's installed solc)
+    // }
 };
